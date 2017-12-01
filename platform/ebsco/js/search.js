7fee4f9d39c4109d77bf411dec778dd06f89b120
Andrey Zaytsev <za@zalab.net>
2017-12-01 15:45:31 +0300

    dev

--- platform/ebsco/js/search.js
+++ platform/ebsco/js/search.js
@@ -1,53 +1,9 @@
 (function(global) {
-    var Credo = global.Credo || (global.Credo = {});
-    
-    var BASE_URL = Credo.config && Credo.config.baseUrl || '//platform.credoreference.com/sdk.js';
-    var SDK_SCRIPT_URL = BASE_URL + '/sdk.js';
-    var WIDGET_STYLES_URL = BASE_URL + '/search/css/search.css';
-
-
     // get reference to initializing script tag
-    var script = (function() {
-        var scripts = document.getElementsByTagName('script');
-        return scripts[scripts.length - 1];
-    })();
-
-    function init() {
-        var target = document.createElement('div');
-        target.id = Credo.sdk.uniqueId();
-        script.parentNode.insertBefore(target, script);
-
-        Credo.sdk.attachStyles(WIDGET_STYLES_URL);
-
-        Credo.init('search', function() {
-            var params = Credo.sdk.parseQuery(script.src);
-            var query = cleanSearchQuery(params.query || params.q || params.searchTerm || params.searchPhrase);
-            if (query) {
-                Credo.SearchWidget({
-                    query: query,
-                    target: target.id,
-                    institutionId: params.institutionId || params.id || params.apiKey
-                });
-            }
-        });
-    }
-
-    function cleanSearchQuery(q) {
-        return q && q.replace(/(^| )(TI|AU|TX|SU|SO|AB|IS|IB)( |$)/g, ' ').trim();
-    }
-
-    if (Credo.sdk) {
-        init();
-    }
-    else {
-        Credo.onReady = Credo.onReady || {};
-        Credo.onReady.sdk = Credo.onReady.sdk || [];
-        Credo.onReady.sdk.push(init);
-
-        var sdkScript = document.createElement('script');
-        sdkScript.type = 'text/javascript';
-        sdkScript.src = SDK_SCRIPT_URL;
-        sdkScript.async = true;
-        document.body.appendChild(sdkScript);
-    }
+    var scripts = document.getElementsByTagName('script');
+    var script = scripts[scripts.length - 1];
+    var searchScript = document.createElement('script');
+    searchScript.src = script.src.replace(/\/ebsco\/search\.js/i, '/search/search.js');
+    searchScript.async = false;
+    script.parentElement.appendChild(searchScript);
 })(this);
