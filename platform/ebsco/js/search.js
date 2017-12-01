1c0a7894dd5bf184471abab8af11e7192b7c4864
Andrey Zaytsev <za@zalab.net>
2017-12-01 15:57:07 +0300

    add ebsco

--- platform/ebsco/js/search.js
+++ platform/ebsco/js/search.js
@@ -3,7 +3,7 @@
     var scripts = document.getElementsByTagName('script');
     var script = scripts[scripts.length - 1];
     var searchScript = document.createElement('script');
-    searchScript.src = script.src.replace(/\/ebsco\/js\/search\.js/i, '/search/js/search.js');
+    searchScript.src = script.src.replace(/\/ebsco\/js\/search\.js/i, '/search/js/search.js') + '&ebsco=1';
     searchScript.async = false;
     script.parentElement.appendChild(searchScript);
 })(this);
